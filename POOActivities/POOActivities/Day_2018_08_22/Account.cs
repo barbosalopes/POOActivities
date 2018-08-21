2669d7b919d7fca2b7241837b961ff2e4cde6e60
barbosalopes <barbosalopesdeveloper@gmail.com>
2018-08-20 22:13:41 -0300

    Upgrade value Account

--- POOActivities/POOActivities/Day_2018_08_22/Account.cs
+++ POOActivities/POOActivities/Day_2018_08_22/Account.cs
@@ -1,18 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
 namespace POOActivities.Day_2018_08_22
 {
+    public struct Operation
+    {
+        public bool isDeposit;
+        public double value;
+        public DateTime date;
+    }
+
     public class Account
     {
-        public struct Operation{
-            public char type;
-            public double value;
-            public DateTime date;
+        List<Operation> operations;
+        private double balance;
 
+        public Account(){
+            operations = new List<Operation>();
         }
 
-        public Account()
-        {
-            
+        public double GetBalance(){
+            return 0;
+        }
+        
+        public Operation[] GetAccountStatement(){
+            return null;
+        }
+
+        public override string ToString(){
+            StringBuilder accountString = new StringBuilder();
+            accountString.AppendLine("Total Balance: " + balance);
+
+            accountString.AppendLine("Operations:");
+            foreach(Operation operation in operations){
+                accountString.AppendLine();
+                accountString.Append(operation.isDeposit ? "Deposit" : "Claim");
+                accountString.Append(operation.value);
+            }
+            return accountString.ToString();
         }
     }
 }
