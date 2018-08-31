d93a96e9f10d8c9147d7323c66b1a422dc8fecb8
barbosalopes <barbosalopesdeveloper@gmail.com>
2018-08-30 22:18:11 -0300

    Insert document class and diagram created.

---
+++ POOActivities/POOActivities/Day_2018_08_30/Documento.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOActivities.Day_2018_08_30
+{
+    class Documento
+    {
+        private static double PrcDocColorido { get; }
+
+        private static double PrcDocPreto { get; }
+
+        private int QntPaginas;
+
+        private char Tipo;
+
+        public Documento(int qntPaginas, char tipo){
+            if(qntPaginas < 0){
+                throw new Exception("Invalid amount of pages.");
+            }
+
+            if()
+            QntPaginas = qntPaginas;
+            Tipo = tipo;
+        }
+    }
+}
